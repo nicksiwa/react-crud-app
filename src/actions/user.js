3439a46d16eb1b5733d65acaa0c69888f5f01ff5
Siwa Khongsuphap <siwa@hospital-os.com>
2018-09-11 11:05:51 +0700

    [USER] Connect service
    - Complete crud service connect
    - Complete redux

--- src/actions/user.js
+++ src/actions/user.js
@@ -11,6 +11,62 @@ export const getAllUser = () => async (dispatch) => {
       })
     }
   } catch (err) {
-    console.log('ACTION_GET_ALL_USER', err)
+    console.log('ACTION_GET_ALL_USER:', err)
+  }
+}
+
+export const getUserById = (id) => async (dispatch) => {
+  try {
+    const res = await Service.User.getUserById(id)
+    if(res) {
+      dispatch({
+        type: type.USER.GET_BY_ID,
+        payload: res.data
+      })
+    }
+  } catch (err) {
+    console.log('ACTION_GET_USER_BY_ID:', err)
+  }
+}
+
+export const createUser = (value) => async (dispatch) => {
+  try {
+    const res = await Service.User.createUser(value)
+    if(res) {
+      dispatch({
+        type: type.USER.CREATE,
+        payload: res.data
+      })
+    }
+  } catch (err) {
+    console.log('ACTION_CREATE_USER:', err)
+  }
+}
+
+export const editUser = (value) => async (dispatch) => {
+  try {
+    const res = await Service.User.editUser(value)
+    if(res) {
+      dispatch({
+        type: type.USER.EDIT,
+        payload: res.data
+      })
+    }
+  } catch (err) {
+    console.log('ACTION_EDIT_USER:', err)
+  }
+}
+
+export const deleteUser = (id) => async (dispatch) => {
+  try {
+    const res = await Service.User.deleteUser(id)
+    if(res) {
+      dispatch({
+        type: type.USER.DELETE,
+        payload: res.config.id
+      })
+    }
+  } catch (err) {
+    console.log('ACTION_DELETE_USER:', err)
   }
 }
--- src/service/user.js
+++ src/service/user.js
@@ -5,7 +5,35 @@ export default {
     try {
       return await call().then((call) => call.get('/users'))
     } catch (err) {
-      console.log('SERVICE_GET_ALL_USER', err)
+      console.log('SERVICE_GET_ALL_USER:', err)
+    }
+  },
+  getUserById: async (id) => {
+    try {
+      return await call().then((call) => call.get(`/users/${id}`))
+    } catch (err) {
+      console.log('SERVICE_GET_USER_BY_ID:', err)
+    }
+  },
+  createUser: async (value) => {
+    try {
+      return await call().then((call) => call.post('/users', value))
+    } catch (err) {
+      console.log('SERVICE_CREATE_USER:', err)
+    }
+  },
+  editUser: async (value) => {
+    try {
+      return await call().then((call) => call.put(`/users/${value.id}`, value))
+    } catch (err) {
+      console.log('SERVICE_EDIT_USER:', err)
+    }
+  },
+  deleteUser: async (id) => {
+    try {
+      return await call().then((call) => call.delete(`/users/${id}`, {id: id}))
+    } catch (err) {
+      console.log('SERVICE_DELETE_USER:', err)
     }
   }
 }
